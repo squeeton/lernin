459ae9f8401f6ee0a5a0202c46b5bd941aeb4089
squeeton <30373837+squeeton@users.noreply.github.com>
2020-09-23 15:43:18 -0500

    Updates

---
+++ precache-manifest.aaabc39584888cc3a588194ab156d63a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f25ecbba1b7567ed3eb5be3b1b62c5a1",
+    "url": "/lernin/index.html"
+  },
+  {
+    "revision": "812821763e8133d5f84d",
+    "url": "/lernin/static/css/main.92b9fa98.chunk.css"
+  },
+  {
+    "revision": "77b7f1f09a124d3892c5",
+    "url": "/lernin/static/js/2.7a4f9669.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lernin/static/js/2.7a4f9669.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "812821763e8133d5f84d",
+    "url": "/lernin/static/js/main.e6a063b0.chunk.js"
+  },
+  {
+    "revision": "f3b789c8782315978291",
+    "url": "/lernin/static/js/runtime-main.97f3e18b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/lernin/static/media/logo.5d5d9eef.svg"
+  }
+]);
